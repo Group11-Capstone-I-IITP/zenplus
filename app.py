2f701cbbcfe9b311bd6ed499bdf6013ced3120d6
Al Ameen N <alameenallu7306@gmail.com>
2026-03-01 07:14:19 +0530

    Al ameen app.py, helpers.py, zenpulse_db.py, apology.html

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-from helpers import login_required
+from helpers import login_required, apology
 
 # Configure application
 app = Flask(__name__)
@@ -15,20 +15,83 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-# db = SQL("sqlite:///zenpulse.db")
+db = SQL("sqlite:///zenpulse.db")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html", name="everyone")
+    """Log user in"""
+    # Forget any user_id
+    session.clear()
 
-# @app.route()
-# def registration():
-#     ...
+    if request.method == "POST":
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+        
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("password")
+        )
 
-# @app.route()
-# def logout():
-#     ...
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username/password", 400)
+        
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
 
-# @app.route()
-# def index():
-#     ...
+        # Redirect user to home page
+        return redirect("/")
+    else:
+        return render_template("login.html")
+
+@app.route("/registration", methods=["GET", "POST"])
+def registration():
+    """Register user"""
+    if request.method == "POST":
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+        elif not request.form.get("confirmation"):
+            return apology("must provide confirmation", 400)
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("passwords do not match", 400)
+        
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username is not already taken
+        if len(rows) == 0:
+            new_id = db.execute(
+                "INSERT INTO users (username, hash) VALUES(?, ?)",
+                request.form.get("username"), generate_password_hash(request.form.get("password"))
+            )
+
+            # Remember which user has logged in
+            session["user_id"] = new_id
+
+            return redirect("/")
+        # username already exists
+        else:
+            return apology("username already exists", 400)
+        
+    return render_template("registration.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+    
+    return redirect("/")
+
+@app.route("/")
+@login_required
+def index():
+    """Show homepage"""
+    return render_template("index.html")
--- zenpulse_db.py
+++ zenpulse_db.py
@@ -2,17 +2,20 @@ import sqlite3
 
 conn = sqlite3.connect('screenbook.db')
 c = conn.cursor()
-#------------------------------------------------#
+#--------------------------------------------------------------------------------------------#
+
+# users tabel
+c.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    username TEXT NOT NULL,
+    hash TEXT NOT NULL,
+)
+''')
 
 
-# USERS TABLE
-# -> id
-# -> username
-# -> hash
 
-
-
-#------------------------------------------------#
+#--------------------------------------------------------------------------------------------#
 conn.commit()
 conn.close()
 print("Database created successfully!")
